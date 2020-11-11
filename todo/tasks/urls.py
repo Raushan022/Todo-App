1d6a5f2370e5401aeb2ea9d69449d923b2a70b73
Raushan <kraushan022@gmail.com>
2020-11-11 21:15:24 +0530

    Added todo project

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -2,6 +2,8 @@ from tasks import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='list'),
+    path('update_task/<str:pk>/', views.updateTask, name='update_task'),
+    path('delete/<str:pk>/', views.deleteTask, name='delete'),
     
 ]
